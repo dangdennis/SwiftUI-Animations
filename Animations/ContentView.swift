648e12b877d33a14051ae2ef3e400b15b83dd682
Dennis Dang <dangggdennis@gmail.com>
2019-10-28 22:18:24 -0400

    Basic animation of button with overlay

---
+++ Animations/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Dennis Dang on 10/28/19.
+//  Copyright © 2019 Dennis Dang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
+    
+    var body: some View {
+        Button("Tap Me") {
+            // self.animationAmount += 1
+        }
+        .padding(40)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation.easeOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+        .onAppear {
+            self.animationAmount = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
